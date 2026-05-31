95fd7eedf27cdc1fb7030e695751c143ecb01304
TheTarnished1 <pascalchidozie986@gmail.com>
2026-05-31 23:04:28 +0100

    api.py

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://paradigm-frontend.vercel.app"], # Change this to your Vercel URL later for security
-    allow_credentials=True
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
